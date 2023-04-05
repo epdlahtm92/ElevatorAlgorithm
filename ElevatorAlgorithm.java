f71e611c9a43bd0a280e8934111adaf4672336df
epdlahtm92 <epdlahtm92@gmail.com>
2023-04-05 18:47:49 +0900

    add floor heights and watt per heights

--- ElevatorAlgorithm.java
+++ ElevatorAlgorithm.java
@@ -7,7 +7,9 @@ public class ElevatorAlgorithm {
 	public static int elev1 = 3;
 	public static int elev2 = 10;
 	public final static int wattPerFloor = 10;
-
+	public final static int[] floorHeights = {15,10,10,10,10,10,10,10,10,15,5};
+	public final static int wattPerHeight = 4;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -17,9 +19,11 @@ public class ElevatorAlgorithm {
 		int distance1 = DistanceCalculator(elev1, currentFloor);
 		int distance2 = DistanceCalculator(elev2, currentFloor);	
 
-		int power1 = EnergyCalculator(distance1, elev1, currentFloor);
-		int power2 = EnergyCalculator(distance2, elev2, currentFloor);
+//		int power1 = EnergyCalculator(distance1, elev1, currentFloor);
+//		int power2 = EnergyCalculator(distance2, elev2, currentFloor);
 
+		int power1 = HeightCalculator(elev1, currentFloor);
+		int power2 = HeightCalculator(elev2, currentFloor);
 		
 		ScreenOutput(power1, power2);
 	}
@@ -30,6 +34,23 @@ public class ElevatorAlgorithm {
 			distance = (elevator - floor) * (-1);
 		}
 		return distance;
+	}
+	
+	public static int HeightCalculator(int elevator, int currentFloor) {
+		int res;
+		int sumHeight = 0;
+		if ((elevator - currentFloor) > 0) {
+			for (int i = (currentFloor - 1); i < (elevator - 1); i++) {
+				sumHeight = sumHeight + floorHeights[i];
+			}
+			res = sumHeight * (wattPerHeight / 2);
+		} else {
+			for (int i = (elevator - 1); i < (currentFloor - 1); i++) {
+				sumHeight = sumHeight + floorHeights[i];
+			}
+			res = sumHeight * wattPerHeight;
+		}
+		return res;
 	}
 	
 	public static void ScreenOutput( int power1, int power2) {	
