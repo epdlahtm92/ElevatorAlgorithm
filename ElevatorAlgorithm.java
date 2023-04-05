af62bbf1d6c3fa4eee24a4fd5edf068e8a264556
epdlahtm92 <epdlahtm92@gmail.com>
2023-04-05 18:32:51 +0900

    add energy calculation

--- ElevatorAlgorithm.java
+++ ElevatorAlgorithm.java
@@ -6,6 +6,7 @@ public class ElevatorAlgorithm {
 	public final int totalFloor = 10;
 	public static int elev1 = 3;
 	public static int elev2 = 10;
+	public final static int wattPerFloor = 10;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,7 +17,11 @@ public class ElevatorAlgorithm {
 		int distance1 = DistanceCalculator(elev1, currentFloor);
 		int distance2 = DistanceCalculator(elev2, currentFloor);	
 
-		ScreenOutput(direction, distance1, distance2);
+		int power1 = EnergyCalculator(distance1, elev1, currentFloor);
+		int power2 = EnergyCalculator(distance2, elev2, currentFloor);
+
+		
+		ScreenOutput(power1, power2);
 	}
 	
 	public static int DistanceCalculator(int elevator, int floor) {
@@ -27,20 +32,33 @@ public class ElevatorAlgorithm {
 		return distance;
 	}
 	
-	public static void ScreenOutput(boolean direction, int distance1, int distance2) {
-		
-		if (direction = true) {
-			if (distance1 > distance2) {
-				System.out.println("call Elevator1");
-			} else {
-				System.out.println("call Elevaotr2");
-			}
+	public static void ScreenOutput( int power1, int power2) {	
+//		if (direction = true) {
+//			if (distance1 > distance2) {
+//				System.out.println("call Elevator1");
+//			} else {
+//				System.out.println("call Elevaotr2");
+//			}
+//		} else {
+//			if (elev1 > elev2) {
+//				System.out.println("call Elevator1");
+//			} else {
+//				System.out.println("call Elevator2");
+//			} 
+//		}
+		if (power1 > power2) {
+			System.out.println("call Elevator1");
 		} else {
-			if (elev1 > elev2) {
-				System.out.println("call Elevator1");
-			} else {
-				System.out.println("call Elevator2");
-			} 
+			System.out.println("call Elevator2");
 		}
+	}
+	
+	public static int EnergyCalculator(int distance, int elevator, int currentFloor) {
+		int res;
+		res = distance * wattPerFloor;
+		if (elevator > currentFloor) {
+			res = res/2;
+		}
+		return res;
 	}
 }
